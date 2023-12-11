e1d8a7c70f5e2935622f3859927834641e314b46
Jared Taylor <44759734+jwt0004@users.noreply.github.com>
2023-12-11 17:17:08 -0600

    Update plot_rs3173615_data.R
    add x-axis labels

--- plot_rs3173615_data.R
+++ plot_rs3173615_data.R
@@ -6,12 +6,12 @@ snp_genotype_by_pop <- read.delim("~/Downloads/snp_genotype_by_population.sorted
 gender_plot <- ggplot(snp_genotype_by_gender, aes(x = rs3173615.Genotype, y = Freq, fill = Gender)) + 
 	geom_bar(stat = "identity", position = position_dodge()) + 
 	scale_fill_manual(values = c("female" = "#56B4E9", "male" = "#E69F00")) + 
-	labs(x = "", y = "Counts", fill = "Gender", title = "rs3173615 Genotype Counts by Gender") +
+	labs(x = "Genotype", y = "Counts", fill = "Gender", title = "rs3173615 Genotype Counts by Gender") +
 	theme_minimal()
   
 population_plot <- ggplot(snp_genotype_by_pop, aes(x = Population, y = Freq, fill = rs3173615.Genotype)) + 
 	geom_bar(stat = "identity", position = position_dodge()) + 
-	labs(x = "", y = "Counts", fill = "Genotype", 
+	labs(x = "Population", y = "Counts", fill = "Genotype", 
 	title = "rs3173615 Genotype Counts by Population") + 
 	scale_fill_manual(values = c("0/0" = "#56B4E9", "0/1" = "#E69F00", "1/1" = "#009E73")) + 
 	theme_minimal() + 
